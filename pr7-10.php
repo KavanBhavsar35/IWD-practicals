53e3248ea3ee21c1c6d9b06656ca8e08b8734320
Jatin Kevlani <119515986+JatinKevlani@users.noreply.github.com>
2024-04-07 13:42:39 +0530

    Update pr7-10.php

--- pr7-10.php
+++ pr7-10.php
@@ -19,25 +19,6 @@ class Circle extends Shape
     }
 }
 
-class Rectangle extends Shape
-{
-    private $length;
-    private $width;
-
-    public function __construct($length, $width)
-    {
-        $this->length = $length;
-        $this->width = $width;
-    }
-
-    public function calculateArea()
-    {
-        return $this->length * $this->width;
-    }
-}
-
 $circle = new Circle(5);
 echo "Area of circle: " . $circle->calculateArea() . "<br>";
-$rectangle = new Rectangle(4, 6);
-echo "Area of rectangle: " . $rectangle->calculateArea();
 ?>
