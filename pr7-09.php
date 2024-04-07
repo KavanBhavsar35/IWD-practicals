eb056cdbd3311837fc0995a7e67ceda1699444bf
Jatin Kevlani <119515986+JatinKevlani@users.noreply.github.com>
2024-04-07 13:40:55 +0530

    Update pr7-09.php

--- pr7-09.php
+++ pr7-09.php
@@ -1,4 +1,4 @@
-<?php
+p<?php
 interface Shape
 {
     public function calculateArea();
@@ -19,25 +19,6 @@ class Circle implements Shape
     }
 }
 
-class Rectangle implements Shape
-{
-    private $length;
-    private $width;
-
-    public function __construct($length, $width)
-    {
-        $this->length = $length;
-        $this->width = $width;
-    }
-
-    public function calculateArea()
-    {
-        return $this->length * $this->width;
-    }
-}
-
 $circle = new Circle(5);
 echo "Area of circle: " . $circle->calculateArea() . "<br>";
-$rectangle = new Rectangle(4, 6);
-echo "Area of rectangle: " . $rectangle->calculateArea();
 ?>
